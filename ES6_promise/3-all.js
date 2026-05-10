b19d2a4dd206f8b22968defe482f2f847c3ef1dd
Hamsa1913 <Hamsa12021@gmail.com>
2026-05-10 20:58:47 +0300

    Refactor imports and update variable names in handleProfileSignup

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,9 @@
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([body, user]) => {
-      console.log(`${body.body} ${user.firstName} ${user.lastName}`);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
